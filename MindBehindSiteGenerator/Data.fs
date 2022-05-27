d14c66eb76d88ae5f021cae3c34b4733bab5a404
Paweł Wicher <pawel.wicher@pse.pl>
2022-05-27 11:05:20 +0200

    foo

---
+++ MindBehindSiteGenerator/Data.fs
@@ -0,0 +1,285 @@
+﻿namespace MindBehindSiteGenerator
+
+type Entry = {
+    Id: string
+    Names: string seq
+}
+
+module Data =
+    
+    let create id names =
+        { Id = id; Names = names |> Seq.ofList }
+
+    let private entry_2022_02_20 =
+        create
+            "2022_02_20"
+            [
+              "go_with_the_flow"
+              "klamstwo_to_nowa_prawda"
+              "klamstwo_to_nowa_prawda_2"
+            ]
+
+    let private entry_2022_01_16 =
+        create
+            "2022_01_16"
+            [
+              "last_caress"
+            ]
+
+    let private entry_2022_01_09 =
+        create
+            "2022_01_09"
+            [
+              "all_my_friends_are_dead"
+              "tata_dilera"
+              "wyrocznia"
+            ]
+
+    let private entry_2021_12_12 =
+        create
+            "2021_12_12"
+            [
+              "go_with_the_flow"
+              "breaking_the_law"
+              "longview"
+              "self_esteem"
+            ]
+
+    let private entry_2021_11_07 =
+        create
+            "2021_11_07"
+            [
+              "paranoid"
+              "marchewkowe_pole"
+              "all_my_friends_are_dead"
+            ]
+
+    let private entry_2021_06_13 =
+        create
+            "2021_06_13"
+            [
+              "go_with_the_flow"
+              "little_sister"
+              "zostac_swirem"
+            ]
+
+    let private entry_2021_06_06 =
+        create
+            "2021_06_06"
+            [
+              "basket_case"
+              "little_sister"
+              "all_my_friends_are_dead"
+              "screamager"
+              "wyrocznia"
+              "paranoid"
+            ]
+
+    let private entry_2021_05_17 =
+        create
+            "2021_05_17"
+            [
+              "all_my_friends_are_dead"
+            ]
+
+    let private entry_2021_05_09 =
+        create
+            "2021_05_09"
+            [
+              "song_2"
+              "all_my_friends_are_dead"
+              "another_brick"
+              "breaking_the_law"
+              "rozmowy_o_muzyce"
+              "screamager"
+              "basket_case"
+              "cocaine"
+            ]
+
+    let private entry_2021_05_01 =
+        create
+            "2021_05_01"
+            [
+              "basket_case"
+              "breed"
+              "all_my_friends_are_dead"
+            ]
+
+    let private entry_2021_04_25 =
+        create
+            "2021_04_25"
+            [
+              "little_sister"
+              "wyrzutek"
+              "wyrocznia"
+            ]
+
+    let private entry_2021_04_18 =
+        create
+            "2021_04_18"
+            [
+              "tata_dilera"
+              "breaking_the_law"
+              "all_my_friends_are_dead"
+            ]
+
+    let private entry_2021_04_11 =
+        create
+            "2021_04_11"
+            [
+              "cocaine"
+              "god_save"
+              "last_caress"
+            ]
+
+    let private entry_2021_02_20 =
+        create
+            "2021_02_20"
+            [
+              "breaking_the_law"
+              "rape_me"
+              "seven_nation_army"
+            ]
+
+    let private entry_2021_02_14 =
+        create
+            "2021_02_14"
+            [
+              "breaking_the_law"
+              "cocaine"
+              "little_sister"
+              "paranoid"
+              "screamager"
+              "wyrzutek"
+            ]
+
+    let private entry_2021_02_06 =
+        create
+            "2021_02_06"
+            [
+              "screamager"
+              "kids_are_not_alright"
+              "zostac_swirem"
+              "song_2"
+            ]
+
+    let private entry_2021_01_31 =
+        create
+            "2021_01_31"
+            [
+              "last_caress"
+              "breaking_the_law"
+              "sic"
+            ]
+
+    let private entry_2021_01_24 =
+        create
+            "2021_01_24"
+            [
+              "last_caress"
+              "kids_are_not_alright"
+              "another_brick"
+            ]
+
+    let private entry_2021_01_17 =
+        create
+            "2021_01_17"
+            [
+              "another_brick"
+            ]
+
+    let private entry_2021_01_10 =
+        create
+            "2021_01_10"
+            [
+              "basket_case"
+              "go_with_the_flow"
+              "kids_are_not_alright"
+            ]
+
+    let private entry_2021_01_03 =
+        create
+            "2021_01_03"
+            [
+              "paranoid"
+              "rape_me"
+              "tata_dilera"
+              "where_is_my_mind"
+              "wyrocznia"
+              "wyrzutek"
+            ]
+
+    let private entry_2020_12_27 =
+        create
+            "2020_12_27"
+            [
+              "breed"
+              "jingle_bells"
+              "little_sister"
+              "rape_me"
+              "screamager"
+              "song_2"
+              "to_co_czujesz"
+              "wyrocznia"
+            ]
+
+    let private entry_2020_12_18 =
+        create
+            "2020_12_18"
+            [
+              "basket_case"
+              "breaking_the_law"
+              "kiedy_bylem"
+              "paranoid"
+            ]
+
+    let private entry_2020_12_13 =
+        create
+            "2020_12_13"
+            [
+              "breaking_the_law"
+              "where_is_my_mind"
+              "zostac_swirem"
+            ]
+
+    let private entry_2020_12_06 =
+        create
+            "2020_12_06"
+            [
+              "basket_case"
+              "breed"
+              "god_save"
+              "kiedy_bylem"
+              "seven_nation_army"
+              "sic"
+              "tata_dilera"
+            ]
+
+    let Entries =
+        [
+            entry_2022_02_20
+            entry_2022_01_16
+            entry_2022_01_09
+            entry_2021_12_12
+            entry_2021_11_07
+            entry_2021_06_13
+            entry_2021_06_06
+            entry_2021_05_17
+            entry_2021_05_09
+            entry_2021_05_01
+            entry_2021_04_25
+            entry_2021_04_18
+            entry_2021_04_11
+            entry_2021_02_20
+            entry_2021_02_14
+            entry_2021_02_06
+            entry_2021_01_31
+            entry_2021_01_24
+            entry_2021_01_17
+            entry_2021_01_10
+            entry_2021_01_03
+            entry_2020_12_27
+            entry_2020_12_18
+            entry_2020_12_13
+            entry_2020_12_06
+        ] |> Seq.ofList
--- MindBehindSiteGenerator/Program.fs
+++ MindBehindSiteGenerator/Program.fs
@@ -1,8 +1,69 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.Text
+open System.IO
 
-open System
+open MindBehindSiteGenerator
+
+let createIndex () =
+    let sb = StringBuilder()
+
+    sb.AppendLine("<!DOCTYPE html>") |> ignore
+    sb.AppendLine("<html lang=\"en\">") |> ignore
+    sb.AppendLine("<meta charset=\"UTF-8\">") |> ignore
+    sb.AppendLine("<title>Mind Behind</title>") |> ignore
+    sb.AppendLine("<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">") |> ignore
+    sb.AppendLine("<link rel=\"icon\" type=\"image/x-icon\" href=\"favicon.ico\">") |> ignore
+    sb.AppendLine("<link rel=\"stylesheet\" href=\"app.css\">") |> ignore
+    sb.AppendLine("<body>") |> ignore
+
+    sb.AppendLine("<h1>Mind Behind</h1>") |> ignore
+
+    sb.AppendLine("<ul>") |> ignore
+    for entry in Data.Entries do
+        sb.AppendLine("<li>") |> ignore
+        sb.AppendLine($"<p>{entry.Id}</p>") |> ignore
+        sb.AppendLine("<ol>") |> ignore
+        for name in entry.Names do
+            sb.AppendLine("<li>") |> ignore
+            sb.AppendLine($"<a href=\"{entry.Id}_{name}.html\" target=\"_blank\">{name}</a>") |> ignore
+            sb.AppendLine("</li>") |> ignore
+        sb.AppendLine("</ol>") |> ignore
+        sb.AppendLine("</li>") |> ignore
+    sb.AppendLine("</ul>") |> ignore
+
+    sb.AppendLine("</body>") |> ignore
+    sb.AppendLine("</html>") |> ignore
+
+    File.WriteAllText(@"C:\Users\ca190\Desktop\output\index.html", sb.ToString())
+
+let createPages () =
+    let sb = StringBuilder()
+
+    for entry in Data.Entries do
+        for name in entry.Names do
+            sb.AppendLine("<!DOCTYPE html>") |> ignore
+            sb.AppendLine("<html lang=\"en\">") |> ignore
+            sb.AppendLine("<meta charset=\"UTF-8\">") |> ignore
+            sb.AppendLine("<title>Mind Behind</title>") |> ignore
+            sb.AppendLine("<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">") |> ignore
+            sb.AppendLine("<link rel=\"icon\" type=\"image/x-icon\" href=\"favicon.ico\">") |> ignore
+            sb.AppendLine("<link rel=\"stylesheet\" href=\"app.css\">") |> ignore
+            sb.AppendLine("<body>") |> ignore
+
+            sb.AppendLine($"<h1>{entry.Id} {name}</h1>") |> ignore
+
+            sb.AppendLine("<video width=\"720\" height=\"480\" controls>") |> ignore
+            sb.AppendLine($"<source src=\"https://github.com/MindBehind2021/{entry.Id}/blob/main/{entry.Id}_{name}.mp4?raw=true\" type=\"video/mp4\">") |> ignore
+            sb.AppendLine("</video>") |> ignore
+
+            sb.AppendLine("</body>") |> ignore
+            sb.AppendLine("</html>") |> ignore
+
+            File.WriteAllText($"C:\Users\ca190\Desktop\output\{entry.Id}_{name}.html", sb.ToString())
+            sb.Clear() |> ignore
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    createIndex ()
+    createPages ()
+    0
